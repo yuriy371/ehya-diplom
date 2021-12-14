e36dc6326976a5775b80861d93312f4cf2330d16
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-12-14 22:01:19 +0400

    создание слайдера с темами

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,8 +1,10 @@
 import modileMenu from "./modules/modileMenu"
-
+import sliderSwiper from "./modules/sliderSwiper"
 
 document.addEventListener("click", (e) => {
 	const targ = e.target
 
 	modileMenu(e, targ)
-})
+});
+
+sliderSwiper();
--- #src/js/modules/modileMenu.js
+++ #src/js/modules/modileMenu.js
@@ -2,9 +2,8 @@ const modileMenu = (e, targ) => {
     let menuMob = document.querySelector(".header__nav-wrapper")
 
     if (targ.closest(".menu-button")) {
-        menuMob.classList.toggle("open")
-        document.querySelector("body").classList.toggle("overflow")
-
+        menuMob.classList.toggle("open");
+        document.querySelector("body").classList.toggle("overflow");
     }
 }
 
