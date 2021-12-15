88ce692063db0dfc0b060d7cf1c075623483ddfb
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-12-15 15:27:11 +0400

    создание блока про авторов

--- #src/js/modules/like.js
+++ #src/js/modules/like.js
@@ -1,5 +1,6 @@
 const like = (e, targ) => {
 	let likeBtn = document.querySelectorAll(".recom__like-btn>.icon")
+	let checkBtn = document.querySelectorAll(".author__checkbox>.icon")
 
 	if (targ.closest(".recom__like-btn>.icon")) {
 		likeBtn.forEach(item => {
@@ -7,6 +8,14 @@ const like = (e, targ) => {
 				item.classList.toggle("active")
 			}
 		});
+	} else if (targ.closest(".author__checkbox>.icon")) {
+		checkBtn.forEach(item => {
+			if (targ.closest(".icon") === item) {
+				item.classList.toggle("active")
+			}
+		});
+	} else {
+		return
 	}
 }
 
