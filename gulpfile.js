b1cdbfffc40497c16b335638934d7aad46b6a365
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-12-16 23:41:42 +0400

    создание модалки, подключение видео и мелкие правки

--- gulpfile.js
+++ gulpfile.js
@@ -132,7 +132,7 @@ function images() {
     return src(path.src.img)
         .pipe(
             webp({
-                quality: 70
+                quality: 10
             })
         )
         .pipe(dest(path.build.img))
